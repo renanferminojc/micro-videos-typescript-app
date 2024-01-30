af9b6d7fb60628d0ad0c5f71c8c52b71c916bfa8
Renan Fermino <renanfermino.jc@outlook.com>
2024-01-30 15:27:10 -0300

    feat: add logic to optional category attributes

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -6,7 +6,11 @@ export type CategoryProps = {
 }
 
 export default class Category {
-  constructor(public readonly props: CategoryProps) {}
+  constructor(public readonly props: CategoryProps) {
+    this.description = this.props.description
+    this.is_active = this.props.is_active
+    this.created_at = this.props.created_at
+  }
 
   get name() {
     return this.props.name
@@ -16,12 +20,24 @@ export default class Category {
     return this.props.description
   }
 
+  private set description (description: string) {
+    this.props.description = description || null
+  }
+
   get is_active() {
     return this.props.is_active
   }
-  
+
+  private set is_active (is_active: boolean) {
+    this.props.is_active = is_active || true
+  }
+
   get created_at() {
     return this.props.created_at
+  }
+  
+  private set created_at (created_at: Date) {
+    this.props.created_at = created_at || new Date()
   }
 
 }
