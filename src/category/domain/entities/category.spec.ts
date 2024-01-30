eaea7a6d1a8cb664daada28182c172c1787bfd49
Renan Fermino <renanfermino.jc@outlook.com>
2024-01-30 17:21:32 -0300

    test: add uuid to category prop

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,3 +1,5 @@
+import { validate as uuidValidate} from 'uuid'
+
 import Category from "./category"
 
 const defaultDate = new Date(2024, 1, 30, 0)
@@ -64,5 +66,29 @@ describe('Category constructor', () => {
       created_at: defaultDate
     })
     expect(category.props.created_at).toBeInstanceOf(Date)
+  })
+
+  it('Should validate uuid if was not provided', () => {
+    const category = new Category({ name: defaultCategoryName })
+    expect(category.id).not.toBeNull()
+    expect(uuidValidate(category.id)).toBeTruthy()
+  })
+
+  it('Should validate uuid if null is provided', () => {
+    const category = new Category({ name: defaultCategoryName, id: null })
+    expect(category.id).not.toBeNull()
+    expect(uuidValidate(category.id)).toBeTruthy()
+  })
+
+  it('Should validate uuid if undefined is provided', () => {
+    const category = new Category({ name: defaultCategoryName, id: null })
+    expect(category.id).not.toBeNull()
+    expect(uuidValidate(category.id)).toBeTruthy()
+  })
+
+  it('Should validate uuid if valid uuid is provided', () => {
+    const category = new Category({ name: defaultCategoryName, id: '21eb0c93-cc1e-499f-837c-d26212059cfc' })
+    expect(category.id).not.toBeNull()
+    expect(uuidValidate(category.id)).toBeTruthy()
   })
 })
